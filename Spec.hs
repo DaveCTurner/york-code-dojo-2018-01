fbb3c8342b7afa12d314232806ac7fc799180725
David Turner <david.turner@elastic.co>
2018-01-10 20:26:17 +0000

    Add requirement 2

--- Spec.hs
+++ Spec.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.Numbers.Primes
 import Test.Hspec
 
 main :: IO ()
@@ -10,6 +11,7 @@ main = hspec $ do
     take 100 ([0,2] ++ cycle [0,1]) `shouldGive` 51
     take 100 (cycle [0,-1])         `shouldGive` (-50)
     (3:[1..99])                     `shouldGive` 4953
+    take 100 ([0,2,0,-1] ++ cycle [0,1,0,-1]) `shouldGive` 1
 
   describe "requirement 1: add one if the total is positive and even" $ do
     replicate 100 1         `shouldGive` (-395)
@@ -20,18 +22,26 @@ main = hspec $ do
 
     take 100 (cycle [0,1])          `shouldGive` 51
     take 100 (cycle [0,-1])         `shouldGive` (-50)
-    take 100 ([1,2] ++ cycle [0,1]) `shouldGive` 53
+    take 100 ([2,3] ++ cycle [0,1]) `shouldGive` 55
+
+  describe "requirement 2: add twenty if the result is prime" $ do
+    take 100 ([1,0,1,-1] ++ cycle [0,1,0,-1]) `shouldGive` 1
+    take 100 ([1,0,2,-1] ++ cycle [0,1,0,-1]) `shouldGive` 23
+    take 100 ([1,0,3,-1] ++ cycle [0,1,0,-1]) `shouldGive` 23
+    take 100 ([1,0,5,-1] ++ cycle [0,1,0,-1]) `shouldGive` 25
+    take 100 ([1,0,7,-1] ++ cycle [0,1,0,-1]) `shouldGive` 27
+    take 100 ([1,0,9,-1] ++ cycle [0,1,0,-1]) `shouldGive` 9
 
   describe "requirement 3: five point penalty for repeated scores" $ do
     take 100 (0         :  cycle [0,1]) `shouldGive` 45
     take 100 ([0,1]     ++ cycle [1,0]) `shouldGive` 45
-    take 100 ([0,1,1,1] ++ cycle [0,1]) `shouldGive` 41
-    take 100 ([0,1,1,1] ++ cycle [1,0]) `shouldGive` 37
+    take 100 ([0,7,7,7] ++ cycle [0,1]) `shouldGive` 79
+    take 100 ([0,1,1,1] ++ cycle [1,0]) `shouldGive` 57
 
     ([0,1,2,2]         ++ [4..99]) `shouldGive` 4945
-    ([0,1,2,2,2]       ++ [5..99]) `shouldGive` 4937
+    ([0,1,2,2,2]       ++ [5..99]) `shouldGive` 4957
     ([0,1,2,2,4,5,6,6] ++ [8..99]) `shouldGive` 4939
-    replicate 100 5                `shouldGive` 5
+    replicate 100 5                `shouldGive` 25
     replicate 100 10               `shouldGive` 505
 
 
@@ -89,8 +99,9 @@ main = hspec $ do
 
 
 calculateScore :: [Integer] -> Integer
-calculateScore = incrementIfPositiveAndEven . go
+calculateScore = addTwentyIfPrime . incrementIfPositiveAndEven . go
   where
+  addTwentyIfPrime x = if x > 0 && isPrime x then x + 20 else x
   incrementIfPositiveAndEven x = if even x && x > 0 then x + 1 else x
 
   go [] = 0
